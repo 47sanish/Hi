a7d2defc1aa7dfd7d9e9b309f437c04a3603d549
Shanish <chaudharysanish54@gmail.com>
2021-02-16 09:32:06 +0545

    algrebric

---
+++ power.py
@@ -0,0 +1,3 @@
+base=float(input("enter the base:"))
+power=base**2
+print("the power of base is",power)
---
+++ sample.py
@@ -0,0 +1,3 @@
+fahrenheit=float(input("enter the value of fahrenheit:"))
+celsius=(fahrenheit-32)*5/9
+print("the value is:",celsius)
